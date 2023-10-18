1fcf4a15060f28cac0df7d8635d5230bbb2b6f9d
ori <oriaviad2006@gmail.com>
2023-10-18 17:05:18 +0300

    checking from a diffrent excel missing to compate with

--- duplicates.py
+++ duplicates.py
@@ -3,9 +3,12 @@ from fuzzywuzzy import fuzz
 from datetime import datetime
 
 
+
 def check_similarity(name1, name2):
     # Check if both names are strings
     if isinstance(name1, str) and isinstance(name2, str):
+        if name1 == name2:
+            return True
         components1 = set(name1.split())
         components2 = set(name2.split())
 
@@ -16,48 +19,51 @@ def check_similarity(name1, name2):
             # Check if at least two identical substrings
             if common_substrings >= 2:
                 return True
-
         # Use fuzz ratio to check similarity (including handling typos)
         similarity_ratio = fuzz.ratio(name1, name2)
-
+    
         # You can adjust the threshold based on your needs
-        fuzzy_similarity = similarity_ratio > 85
-        # similarity_ratio > 80  # Adjust the threshold as needed
+        fuzzy_similarity = similarity_ratio > 87
+        #similarity_ratio > 80  # Adjust the threshold as needed
 
         # Regular similarity check
+        
         return fuzzy_similarity
     else:
         return False  # Skip non-string values
 
 # Load the Excel file into a DataFrame
-file_path = 'C:\\Users\\USER\\Desktop\\projects\\hamal\\data\\mondayCopy.xlsx'
-df = pd.read_excel(file_path)
+all_names_path = 'C:\\Users\\USER\\Desktop\\projects\\hamal\\data\\mondayCopy.xlsx'
+df = pd.read_excel(all_names_path)
+names_to_check_path = 'C:\\Users\\USER\\Desktop\\projects\\hamal\\data\\to_check.xlsx'
+data = pd.read_excel(names_to_check_path)
 
 # Get the names from column A
-names = df.iloc[:100, 0].tolist()
+names = data.iloc[:, 0].tolist()
+allNames = df.iloc[:, 0].tolist()
 
-# Dictionary to store similar names
-similar_names_dict = {}
 
+# List to store similar name pairs
+similar_names = []
+count_iterations = 0
 # Iterate through names and check for similarity
 for i in range(len(names)):
-    similar_names_dict[names[i]] = []
-    for j in range(i + 1, len(names)):
-        if check_similarity(names[i], names[j]):
-            # Add similar names to the list
-            similar_names_dict[names[i]].append(names[j])
+    for j in range(i + 1, len(allNames)):
+        count_iterations+=1
+        if check_similarity(names[i], allNames[j]):
+            similar_names.append((names[i], allNames[j], i, j))
+print(count_iterations)
 
 
-# Create a DataFrame for the similar name groups
-output_df = pd.DataFrame(list(similar_names_dict.items()), columns=['MainName', 'SimilarNames'])
+# Create a DataFrame for the similar name pairs
+output_df = pd.DataFrame(similar_names, columns=['Name1', 'Name2', 'name1rowID', 'name2RowID'])
 
-# Sort the DataFrame alphabetically based on 'MainName'
-output_df = output_df.sort_values(by='MainName')
+# Sort the DataFrame alphabetically based on 'Name1'
 
 # Create a new Excel file with the sorted DataFrame
 current_time = datetime.now().time()
 formatted_time = current_time.strftime('%H_%M_%S')
-output_file_path = f'C:\\Users\\USER\\Desktop\\projects\\hamal\\{formatted_time}.xlsx'
+output_file_path = f'C:\\Users\\USER\\Desktop\\projects\\hamal\\data\\{formatted_time}.xlsx'
 output_df.to_excel(output_file_path, index=False)
 
-print(f'Similar names grouped and saved to: {output_file_path}')
+print(f'Similar names saved and sorted to: {output_file_path}')
